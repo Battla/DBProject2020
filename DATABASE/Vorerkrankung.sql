85152b894b75ad34e7b3acd347d83bc5ae326913
Kai <kai.kuehl@outlook.de>
2021-01-14 16:59:27 +0100

    Krankenakte, Vorerkrankung, Diagnose

--- DATABASE/Vorerkrankung.sql
+++ DATABASE/Vorerkrankung.sql
@@ -2,7 +2,7 @@ use krankenhaus;
 
 CREATE TABLE IF NOT EXISTS MRT (
     ICB10Nummer VARCHAR(7),
-    Patientennummer VARCHAR(255),
+    Patientennummer INT,
     Beschreibung VARCHAR(255),
 
     PRIMARY KEY(ICB10Nummer)
