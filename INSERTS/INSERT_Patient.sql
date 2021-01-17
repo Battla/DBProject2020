d5db13e5e433510f828d99bcfff04ca61a832642
kleinhus <steffenkleinhaus@outlook.com>
2021-01-17 18:39:03 +0100

    Hallo

--- INSERTS/INSERT_Patient.sql
+++ INSERTS/INSERT_Patient.sql
@@ -1,39 +1,39 @@
 USE krankenhaus;
-INSERT INTO Patient(Status, Strasse, Hausnummer, PLZ, Geschlecht, Vorname, Nachname, Titel, EMail, Geburtsdatum)
-VALUES('Untersuchung', 'Baumweg', 200, 49743, 'männlich', 'Voker', 'Müller', NULL, 'VMüller@web.de','1988-05-14' );
+INSERT INTO Patient(Status, Strasse, Hausnummer, PLZ, Geschlecht, Vorname, Nachname, Titel, EMail, Geburtsdatum, Station)
+VALUES('Untersuchung', 'Baumweg', 200, 49743, 'männlich', 'Voker', 'Müller', NULL, 'VMüller@web.de','1988-05-14', '3');
 
 USE krankenhaus;
-INSERT INTO Patient( Status, Strasse, Hausnummer, PLZ, Geschlecht, Vorname, Nachname, Titel, EMail, Geburtsdatum)
-VALUES( 'Behandlung', 'Röntgenstraße', 20, 49743, 'weiblich', 'Hilde', 'Jansen', NULL, 'HJ@web.de','1964-03-28' );
+INSERT INTO Patient( Status, Strasse, Hausnummer, PLZ, Geschlecht, Vorname, Nachname, Titel, EMail, Geburtsdatum, Station)
+VALUES( 'Behandlung', 'Röntgenstraße', 20, 49743, 'weiblich', 'Hilde', 'Jansen', NULL, 'HJ@web.de','1964-03-28', '3');
 
 USE krankenhaus;
-INSERT INTO Patient( Status, Strasse, Hausnummer, PLZ, Geschlecht, Vorname, Nachname, Titel, EMail, Geburtsdatum)
-VALUES( 'Untersuchung', 'Gartenstraße', 1, 49743, 'männlich', 'Tobias', 'Giesen', NULL, 'TG1999@web.de','1999-10-25' );
+INSERT INTO Patient( Status, Strasse, Hausnummer, PLZ, Geschlecht, Vorname, Nachname, Titel, EMail, Geburtsdatum, Station)
+VALUES( 'Untersuchung', 'Gartenstraße', 1, 49743, 'männlich', 'Tobias', 'Giesen', NULL, 'TG1999@web.de','1999-10-25', '6');
 
 USE krankenhaus;
-INSERT INTO Patient( Status, Strasse, Hausnummer, PLZ, Geschlecht, Vorname, Nachname, Titel, EMail, Geburtsdatum)
-VALUES( 'Entlassung', 'Baumweg', 124, 49743, 'diverse', 'Günter', 'Häfele', NULL, 'GünterHäfele@web.de','1950-06-01' );
+INSERT INTO Patient( Status, Strasse, Hausnummer, PLZ, Geschlecht, Vorname, Nachname, Titel, EMail, Geburtsdatum, Station)
+VALUES( 'Entlassung', 'Baumweg', 124, 49743, 'diverse', 'Günter', 'Häfele', NULL, 'GünterHäfele@web.de','1950-06-01', '6') ;
 
 USE krankenhaus;
-INSERT INTO Patient( Status, Strasse, Hausnummer, PLZ, Geschlecht, Vorname, Nachname, Titel, EMail, Geburtsdatum)
-VALUES('Untersuchung', 'Zum Buchenwald', 5, 49743, 'weiblich', 'Hella', 'Schafft', NULL, 'HellaSchaft@outlook.com','1933-08-06' );
+INSERT INTO Patient( Status, Strasse, Hausnummer, PLZ, Geschlecht, Vorname, Nachname, Titel, EMail, Geburtsdatum, Station)
+VALUES('Untersuchung', 'Zum Buchenwald', 5, 49743, 'weiblich', 'Hella', 'Schafft', NULL, 'HellaSchaft@outlook.com','1933-08-06', '6') ;
 
 USE krankenhaus;
-INSERT INTO Patient(Status, Strasse, Hausnummer, PLZ, Geschlecht, Vorname, Nachname, Titel, EMail, Geburtsdatum)
-VALUES( 'Behandlung', 'Glockstraße', 22, 49743, 'männlich', 'Harry', 'Mark', NULL, 'HM@gmail.com','1955-11-06' );
+INSERT INTO Patient(Status, Strasse, Hausnummer, PLZ, Geschlecht, Vorname, Nachname, Titel, EMail, Geburtsdatum, Station)
+VALUES( 'Behandlung', 'Glockstraße', 22, 49743, 'männlich', 'Harry', 'Mark', NULL, 'HM@gmail.com','1955-11-06', '6' ) ;
 
 USE krankenhaus;
-INSERT INTO Patient(Status, Strasse, Hausnummer, PLZ, Geschlecht, Vorname, Nachname, Titel, EMail, Geburtsdatum)
-VALUES( 'Entlassung', 'Zum Turm', 66, 49743, 'weiblich', 'Theresa', 'Endres', NULL, 'TEndred@outlook.com','1987-03-08' );
+INSERT INTO Patient(Status, Strasse, Hausnummer, PLZ, Geschlecht, Vorname, Nachname, Titel, EMail, Geburtsdatum, Station)
+VALUES( 'Entlassung', 'Zum Turm', 66, 49743, 'weiblich', 'Theresa', 'Endres', NULL, 'TEndred@outlook.com','1987-03-08', '6') ;
 
 USE krankenhaus;
-INSERT INTO Patient(Status, Strasse, Hausnummer, PLZ, Geschlecht, Vorname, Nachname, Titel, EMail, Geburtsdatum)
-VALUES('Entlassung', 'Schulstraße', 194, 49743, 'weiblich', 'Manuela', 'Koppelkamp', NULL, 'KoppelkampM@gmail.com','1985-09-22' );
+INSERT INTO Patient(Status, Strasse, Hausnummer, PLZ, Geschlecht, Vorname, Nachname, Titel, EMail, Geburtsdatum, Station)
+VALUES('Entlassung', 'Schulstraße', 194, 49743, 'weiblich', 'Manuela', 'Koppelkamp', NULL, 'KoppelkampM@gmail.com','1985-09-22', '6') ;
 
 USE krankenhaus;
-INSERT INTO Patient( Status, Strasse, Hausnummer, PLZ, Geschlecht, Vorname, Nachname, Titel, EMail, Geburtsdatum)
-VALUES('Behandlung', 'Dorfstraße', 69, 49743, 'weiblich', 'Annalena', 'Hellweg', NULL, 'AnnaHellweg@web.de','1997-05-03' );
+INSERT INTO Patient( Status, Strasse, Hausnummer, PLZ, Geschlecht, Vorname, Nachname, Titel, EMail, Geburtsdatum, Station)
+VALUES('Behandlung', 'Dorfstraße', 69, 49743, 'weiblich', 'Annalena', 'Hellweg', NULL, 'AnnaHellweg@web.de','1997-05-03','2');
 
 USE krankenhaus;
-INSERT INTO Patient(Status, Strasse, Hausnummer, PLZ, Geschlecht, Vorname, Nachname, Titel, EMail, Geburtsdatum)
-VALUES('Untersuchung', 'Wasserberg', 1 , 49743, 'männlich', 'Peter', 'Parry', NULL, 'HarryPeter@outlook.com','1988-12-04' );
+INSERT INTO Patient(Status, Strasse, Hausnummer, PLZ, Geschlecht, Vorname, Nachname, Titel, EMail, Geburtsdatum, Station)
+VALUES('Untersuchung', 'Wasserberg', 1 , 49743, 'männlich', 'Peter', 'Parry', NULL, 'HarryPeter@outlook.com','1988-12-04','2');
