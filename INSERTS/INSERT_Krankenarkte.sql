2d61361b0f2873bb69aab4ba211411bb71e327d3
kleinhus <steffenkleinhaus@outlook.com>
2021-01-17 18:33:08 +0100

    Hallo

---
+++ INSERTS/INSERT_Krankenarkte.sql
@@ -0,0 +1,33 @@
+USE krankenhaus;
+INSERT INTO Krankenakte(Aktenzeichen, Patientennummer)
+VALUES('XYGDFS', '');
+
+INSERT INTO Krankenakte(Aktenzeichen, Patientennummer)
+VALUES('DGHFGW', '');
+
+INSERT INTO Krankenakte(Aktenzeichen, Patientennummer)
+VALUES('GDUHCA', '');
+
+INSERT INTO Krankenakte(Aktenzeichen, Patientennummer)
+VALUES('HIDFKH', '');
+
+INSERT INTO Krankenakte(Aktenzeichen, Patientennummer)
+VALUES('HDOLKR', '');
+
+INSERT INTO Krankenakte(Aktenzeichen, Patientennummer)
+VALUES('LIPDSU', '');
+
+INSERT INTO Krankenakte(Aktenzeichen, Patientennummer)
+VALUES('IHRFLG', '');
+
+INSERT INTO Krankenakte(Aktenzeichen, Patientennummer)
+VALUES('FHASOI', '');
+
+INSERT INTO Krankenakte(Aktenzeichen, Patientennummer)
+VALUES('UHRLOK', '');
+
+INSERT INTO Krankenakte(Aktenzeichen, Patientennummer)
+VALUES('OLWKLG', '');
+
+
+
