f40984e67cdbb98fccf41f3b9c8b465b7613dfa2
jurkup <kai.kuehl@outlook.de>
2021-01-18 16:33:05 +0100

    Select

--- INSERTS/INSERT_Station.sql
+++ INSERTS/INSERT_Station.sql
@@ -1,27 +1,27 @@
 USE krankenhaus;
 
 INSERT INTO station(Stationsnummer, Bezeichnung, Anz_Zimmer, Leitung, Leitung_Pfleger)
-VALUES (0, 'Verwaltung', 4, 'n/a', 'n/a');
+VALUES (0, 'Verwaltung', 4, NULL, NULL);
 
 INSERT INTO station(Stationsnummer, Bezeichnung, Anz_Zimmer, Leitung, Leitung_Pfleger)
-VALUES (1, 'Radiologie', 6, 1, '');
+VALUES (1, 'Radiologie', 6, 1, 22);
 
 INSERT INTO station(Stationsnummer, Bezeichnung, Anz_Zimmer, Leitung, Leitung_Pfleger)
-VALUES (2, 'Hals Nasen Ohren', 3, 2, '');
+VALUES (2, 'Hals Nasen Ohren', 3, 2, 14);
 
 INSERT INTO station(Stationsnummer, Bezeichnung, Anz_Zimmer, Leitung, Leitung_Pfleger)
-VALUES (3, 'Chirurgie', 5, 4, '');
+VALUES (3, 'Chirurgie', 5, 4, 13);
 
 INSERT INTO station(Stationsnummer, Bezeichnung, Anz_Zimmer, Leitung, Leitung_Pfleger)
-VALUES (4, 'Orthopädie', 2, 7, '');
+VALUES (4, 'Orthopädie', 2, 7, 19);
 
 INSERT INTO station(Stationsnummer, Bezeichnung, Anz_Zimmer, Leitung, Leitung_Pfleger)
-VALUES (5, 'Dermatologie', 2, 9, '');
+VALUES (5, 'Dermatologie', 2, 9, 17);
 
 INSERT INTO station(Stationsnummer, Bezeichnung, Anz_Zimmer, Leitung, Leitung_Pfleger)
-VALUES (6, 'Notfall', 3, 10, '');
+VALUES (6, 'Notfall', 3, 10, 24);
 
 INSERT INTO station(Stationsnummer, Bezeichnung, Anz_Zimmer, Leitung, Leitung_Pfleger)
-VALUES (7, 'Rheumatologie', 1, 11, '');
+VALUES (7, 'Rheumatologie', 1, 11, 26);
 
 
